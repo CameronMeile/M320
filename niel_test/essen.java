068115111251fb941fb331c7cf47ea69dad448e0
niel987654321 <112867243+niel987654321@users.noreply.github.com>
2023-08-21 13:34:49 +0000

    made calss essen and made default values

---
+++ niel_test/essen.java
@@ -0,0 +1,19 @@
+package niel_test;
+
+public class essen {
+    private int hungerLevel;
+    private int HamburgerHungerLevels;
+    private int EisHungerLevels;
+    private int PommesHungerLevels;
+
+    public essen() {
+        hungerLevel = 100;
+        HamburgerHungerLevels = 10;
+        EisHungerLevels = 2;
+        PommesHungerLevels = 5;
+    }
+
+    
+
+}
+
---
+++ niel_test/main.java
@@ -0,0 +1,9 @@
+package niel_test;
+
+public class main {
+    public static void main (String[] args)
+    {
+          // Ausgabe Hello World!
+          System.out.println("Hello World!");
+    }
+}
