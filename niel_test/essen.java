0c186372f2faf032c2b9c5bbf093c83cb1810a49
niel987654321 <112867243+niel987654321@users.noreply.github.com>
2023-08-28 11:14:13 +0000

    finish niel_test

--- niel_test/essen.java
+++ niel_test/essen.java
@@ -8,12 +8,30 @@ public class essen {
 
     public essen() {
         hungerLevel = 100;
-        HamburgerHungerLevels = 10;
-        EisHungerLevels = 2;
-        PommesHungerLevels = 5;
+        HamburgerHungerLevels = 50;
+        EisHungerLevels = 10;
+        PommesHungerLevels = 30;
     }
 
+    public int gethungerLevel() {
+        return this.hungerLevel;
+    }
+
+    public void sethungerLevel(int newhungerLevel) {
+        this.hungerLevel -= newhungerLevel;
+    }
+
+    public void eatEis() {
+        this.hungerLevel += EisHungerLevels;
+    }
     
+    public void eatHamburger() {
+        this.hungerLevel += HamburgerHungerLevels;
+    }
+
+    public void eatPommes() {
+        this.hungerLevel += PommesHungerLevels;
+    }
 
 }
 
--- niel_test/main.java
+++ niel_test/main.java
@@ -1,9 +1,48 @@
 package niel_test;
 
+import java.util.Scanner;
+
 public class main {
+
+    static essen person = new essen();
+
+
     public static void main (String[] args)
     {
-          // Ausgabe Hello World!
-          System.out.println("Hello World!");
+        Scanner essenfrage = new Scanner(System.in);  // Create a Scanner object
+        while(true){
+            System.out.println("Hunger:"+person.gethungerLevel());
+            String essenAusgewählt =  essenAuswahl(essenfrage);
+            esse(essenAusgewählt);
+            nextloop();
+        }
     }
+
+    public static void nextloop() {
+        person.sethungerLevel(10);
+    }
+
+    public static String essenAuswahl(Scanner essenfrage) {
+        System.out.println("Was wollen sie essen?");
+        String essen = essenfrage.nextLine();  // Read user input
+        return essen;
+    }
+
+    public static void esse(String essenAusgewählt) {
+        switch(essenAusgewählt) {
+            case "Eis":
+            person.eatEis();
+            break;
+            case "Pommes":
+            person.eatPommes();
+            break;
+            case "Hamburger":
+            person.eatHamburger();
+            break;
+            case "nix":
+            break;
+          }
+          
+    }
+
 }
