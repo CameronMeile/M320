bd806a298bb52885c02928a6ff9bc165dbbd9e80
Cameron Meile <cameron.meile@edu.tbz.ch~>
2023-09-18 13:42:51 +0200

    [D2] Updated version of Test file

--- maven_Niel/src/main/java/D2/Test.java
+++ maven_Niel/src/main/java/D2/Test.java
@@ -5,15 +5,14 @@ public class Test {
     int studentPoints;
     int maxPoints;
 
+    public Test(int points, int maxpoints){
+        studentPoints = points;
+        maxPoints = maxpoints;
+    }
+
     public void calculateGrade() {
         float grade = ((float) studentPoints / maxPoints) * 5 + 1;
         System.out.println("Grade: " + grade);
     }
 
-    public static void main(String[] args) {
-        Test test = new Test();
-        test.studentPoints = 80;
-        test.maxPoints = 100;
-        test.calculateGrade();
-    }
 }
