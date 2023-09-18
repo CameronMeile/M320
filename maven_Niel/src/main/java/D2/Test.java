ca3d818023fd82a60579ec045235dc867d9e57a7
Cameron Meile <cameron.meile@edu.tbz.ch~>
2023-09-18 13:36:38 +0200

    [D2] Added calculateGrade

--- maven_Niel/src/main/java/D2/Test.java
+++ maven_Niel/src/main/java/D2/Test.java
@@ -1,4 +1,14 @@
 package maven_Niel.src.main.java.D2;
 
 public class Test {
+
+    int studentPoints;
+    int maxPoints;
+
+    public void calculateGrade() {
+
+        float Grade = studentPoints * 5 / maxPoints + 1;
+
+    }
+
 }
